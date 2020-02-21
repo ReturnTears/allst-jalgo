89b2c62734ec2f378020f6392b2d16daf06271aa
returntears <returntears@outlook.com>
2020-02-22 02:24:24 +0800

    斐波拉契（黄金分割）查找算法

---
+++ src/main/java/com/allst/jvalgo/search/FiboracciSearch.java
@@ -0,0 +1,15 @@
+package com.allst.jvalgo.search;
+
+/**
+ * 斐波拉契查找算法
+ *
+ * @author YiYa
+ * @since 2020-02-22 上午 02:14
+ */
+public class FiboracciSearch {
+
+    public static void main(String[] args) {
+
+    }
+
+}
