05e052c025dcf8cb42fdffac8c27cd43e2a49798
returntears <returntears@outlook.com>
2020-03-05 00:45:03 +0800

    平衡二叉树

---
+++ src/main/java/com/allst/jvalgo/avl/AvlTree.java
@@ -0,0 +1,15 @@
+package com.allst.jvalgo.avl;
+
+/**
+ * 平衡二叉树
+ *
+ * @author YiYa
+ * @since 2020-03-05 上午 12:40
+ */
+public class AvlTree {
+
+    public static void main(String[] args) {
+
+    }
+
+}
