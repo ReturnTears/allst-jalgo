4066e93948a75f615b1a64b4fe8f8dca7fd332c8
returntears <returntears@outlook.com>
2020-03-05 23:19:46 +0800

    平衡二叉树

--- src/main/java/com/allst/jvalgo/avl/AvlTree.java
+++ src/main/java/com/allst/jvalgo/avl/AvlTree.java
@@ -13,3 +13,37 @@ public class AvlTree {
     }
 
 }
+
+class AVLTree {
+    private Node node;
+
+    public Node getNode() {
+        return node;
+    }
+}
+
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                '}';
+    }
+
+    // 左旋转
+    private void leftRotate() {
+        Node newNode = new Node(value);
+        newNode.left = left;
+        newNode.right = right.left;
+        value = right.value;
+        left = newNode;
+    }
+}
