c76bd57b027603890c6d8e77ef10955773798cd4
returntears <returntears@outlook.com>
2020-03-09 00:01:53 +0800

    图

---
+++ src/main/java/com/allst/jvalgo/graph/MyGraph.java
@@ -0,0 +1,100 @@
+package com.allst.jvalgo.graph;
+
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 图
+ *
+ * @author YiYa
+ * @since 2020-03-08 下午 11:33
+ */
+public class MyGraph {
+    //顶点集合
+    private List<String> vertexList;
+    // 存储图对应的邻接矩阵
+    private int[][] edges;
+    // 边的数目
+    private int numOfEdges;
+
+    public MyGraph(int n) {
+        edges = new int[n][n];
+        vertexList = new ArrayList<>(n);
+        numOfEdges = 0;
+    }
+
+    /**
+     * 插入节点
+     * @param vertex
+     */
+    public void insertVertex(String vertex) {
+        vertexList.add(vertex);
+    }
+
+    /**
+     * 边的数目
+     */
+    public int getVertexSize() {
+        return vertexList.size();
+    }
+
+    /**
+     * 节点i对应的数据
+     * @param i
+     * @return
+     */
+    public String getValueByIndex(int i) {
+        return vertexList.get(i);
+    }
+
+    /**
+     * 获取权值
+     * @param v1
+     * @param v2
+     * @return
+     */
+    private int getWeight(int v1, int v2) {
+        return edges[v1][v2];
+    }
+
+    /**
+     * 显示图
+     */
+    public void showGraph() {
+        for (int[] line : edges) {
+            System.out.println(Arrays.toString(line));
+        }
+    }
+
+    /**
+     * 添加边
+     * @param v1
+     * @param v2
+     * @param weight
+     */
+    public void insertEdge(int v1, int v2, int weight) {
+        edges[v1][v2] = weight;
+        edges[v2][v1] = weight;
+        numOfEdges++;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        String[] vertexValue = {"A", "B", "C", "D", "E"};
+        MyGraph graph = new MyGraph(n);
+        for (String ver : vertexValue) {
+            graph.insertVertex(ver);
+        }
+        graph.insertEdge(0, 1, 1);
+        graph.insertEdge(0, 2, 1);
+        graph.insertEdge(1, 2, 1);
+        graph.insertEdge(1, 3, 1);
+        graph.insertEdge(1, 4, 1);
+
+        graph.showGraph();
+    }
+
+}
