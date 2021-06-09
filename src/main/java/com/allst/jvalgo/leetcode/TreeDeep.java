fc9dd27909105de96e373d906fdc6e835172f16d
returntears <returntears@outlook.com>
2021-06-09 23:59:34 +0800

    二叉树的广度优先算法

--- src/main/java/com/allst/jvalgo/leetcode/TreeDeep.java
+++ src/main/java/com/allst/jvalgo/leetcode/TreeDeep.java
@@ -1,8 +1,11 @@
 package com.allst.jvalgo.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- * 二叉树的深度优先算法
- *
+ * 二叉树的深度优先算法：把叶子节点标记为1
+ * 二叉树的广度优先算法：把root节点标记为1
  *
  * @author June
  * @since 2021年06月
@@ -18,13 +21,14 @@ public class TreeDeep {
         TreeNode node1 = new TreeNode(1, node2, node3);
 
         System.out.println(minDept(node1));
+        System.out.println(minDept2(node1));
     }
 
     static class TreeNode {
         int value;
         TreeNode left;
         TreeNode right;
-
+        int deep;
         public TreeNode(int value, TreeNode left, TreeNode right) {
             this.value = value;
             this.left = left;
@@ -33,7 +37,8 @@ public class TreeDeep {
     }
 
     /**
-     * 获取二叉树的最小深度
+     * 获取二叉树的最小深度：深度优先算法
+     * 空间复杂度O(logn)
      *
      * @param root 树节点
      * @return 结果
@@ -53,5 +58,36 @@ public class TreeDeep {
             min = Math.min(minDept(root.right), min);
         }
         return min + 1;
+    }
+
+    /**
+     * 获取二叉树的最小深度: 广度优先算法
+     * 空间复杂度O(n)
+     *
+     * @param root 树节点
+     * @return 结果
+     */
+    private static int minDept2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        root.deep = 1;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left == null && node.right == null) {
+                return node.deep;
+            }
+            if (node.left != null) {
+                node.left.deep = node.deep + 1;
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                node.right.deep = node.deep + 1;
+                queue.offer(node.right);
+            }
+        }
+        return 0;
     }
 }
