59596ae262987050e2dbb5e6551a051191df1ece
returntears <returntears@outlook.com>
2021-08-08 18:40:36 +0800

    判定字符是否唯一

---
+++ src/test/java/com/allst/jvalgo/leetcode2/PdStringUniqueTest.java
@@ -0,0 +1,16 @@
+package com.allst.jvalgo.leetcode2;
+
+import org.junit.Test;
+
+/**
+ * @author June
+ * @since 2021年08月
+ */
+public class PdStringUniqueTest {
+
+    @Test
+    public void testIsUnique() {
+        boolean unique = PdStringUnique.isUnique("leetcode");
+        System.out.println("result: " + unique);
+    }
+}
