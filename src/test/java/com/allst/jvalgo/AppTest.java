deda59b4b52e17df37d1b755b1c7cde6317e8ab2
returntears <returntears@outlook.com>
2022-02-10 23:44:46 +0800

    matcher

--- src/test/java/com/allst/jvalgo/AppTest.java
+++ src/test/java/com/allst/jvalgo/AppTest.java
@@ -2,6 +2,9 @@ package com.allst.jvalgo;
 
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -16,6 +19,15 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+        String line = "<cas:user>admin</cas:user>";
+        String regex = "<cas:user>(.*)</cas:user>";
+        Matcher matcher = Pattern.compile(regex).matcher(line);
+        if (matcher.find()) {
+            System.out.println(matcher.group());
+            System.out.println(matcher.group(0));
+            //提取出的值
+            System.out.println(matcher.group(1));
+        }
     }
 
     @Test
